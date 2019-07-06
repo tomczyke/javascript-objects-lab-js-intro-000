8b7a9e7e15611fb0e9999514c0cbb0321d9b9f1d
Evan Tomczyk <tomczykej@gmail.com>
2019-07-06 19:34:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
+<<<<<<< HEAD
   return Object.assign({}, object, { [key]: value })
 }
 
@@ -20,3 +21,17 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
+=======
+  //recipes[key] = value
+  //return recipes;
+  return Object.assign({}, {key: 'value' })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  //return Object[assign]({}, {key: 'value' });
+  recipes[key] = value
+  return recipes;
+}
+
+//function deleteFromObjectByKey(object, key)
+>>>>>>> f4ddedd42aa2f7f0607584718f0e05d862bb9d01
